f6e6f279259528146bba7d7c2c118e530fa2c7b0
sergio Ugarte <sergiouga85@gmail.com>
2024-01-09 23:50:54 -0300

    primer Commit

---
+++ src/models/mongoose/User.js
@@ -0,0 +1,37 @@
+import { Schema, model } from 'mongoose'
+import { randomUUID } from 'crypto'
+import { hasheadasSonIguales, hashear } from '../../utils/criptografia.js'
+
+const schema = new Schema({
+  _id: { type: String, default: randomUUID },
+  first_name: { type: String, required: true },
+  last_name: { type: String,  required: true },
+  password: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
+  rol: { type: String, default: 'user' },
+  cart: { type: String, required: true }
+}, {
+  versionKey: false,
+  strict: 'throw',
+  statics: {
+    register: async (userData) => {
+      userData.password = hashear(userData.password)
+      const user = await model('users').create(userData)
+      return user.toObject()
+    },
+    login: async ({ email, password }) => {
+      const user = await model('users').findOne({ email })
+      if (!user) { throw new Error('authentication error') }
+      if (!hasheadasSonIguales({
+        recibida: password,
+        almacenada: user.password
+      })) {
+        throw new Error('authentication error')
+      }
+      return user.toObject()
+    },
+
+  }
+})
+
+export const usersManager = model('users', schema)
